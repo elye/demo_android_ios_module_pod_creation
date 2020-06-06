3e01b2444faa03c1e455efe878240876a2b2c7e4
elye <elye.project@gmail.com>
2020-06-06 15:02:11 +1000

    Upgrade to Android X

--- AndroidNetworkExperiment/app/src/main/java/com/elyeproj/networkexperiment/MainActivity.kt
+++ AndroidNetworkExperiment/app/src/main/java/com/elyeproj/networkexperiment/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.elyeproj.networkexperiment
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import com.elyeproj.wikipediafetcher.WikipediaFetcher
 import io.reactivex.disposables.Disposables
